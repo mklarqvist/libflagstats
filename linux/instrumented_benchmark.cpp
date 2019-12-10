4206cef42cfac77f53c39ace7e6953079d1768f8
Marcus D. R. Klarqvist <mk819@cam.ac.uk>
2019-12-10 09:51:25 +0000

    add memcpy

--- linux/instrumented_benchmark.cpp
+++ linux/instrumented_benchmark.cpp
@@ -57,10 +57,35 @@ typedef struct {
     if (c & FLAGSTAT_FDUP) ++(s)->n_dup[w];                 \
 } while (0)
 
+void SAMTOOLS_func(bam_flagstat_t* s, const uint16_t c) {
+    int w = (c & FLAGSTAT_FQCFAIL) ? 1 : 0; 
+    ++(s)->n_reads[w];
+    if (c & FLAGSTAT_FSECONDARY ) {
+        ++(s)->n_secondary[w];
+    } else if (c & FLAGSTAT_FSUPPLEMENTARY ) {
+        ++(s)->n_supp[w];
+    } else if (c & FLAGSTAT_FPAIRED) {
+        ++(s)->n_pair_all[w];
+        if ( (c & FLAGSTAT_FPROPER_PAIR) && !(c & FLAGSTAT_FUNMAP) ) ++(s)->n_pair_good[w];
+        if (c & FLAGSTAT_FREAD1) ++(s)->n_read1[w];
+        if (c & FLAGSTAT_FREAD2) ++(s)->n_read2[w];
+        if ((c & FLAGSTAT_FMUNMAP) && !(c & FLAGSTAT_FUNMAP)) ++(s)->n_sgltn[w];
+        if (!(c & FLAGSTAT_FUNMAP) && !(c & FLAGSTAT_FMUNMAP)) {
+            ++(s)->n_pair_map[w];
+        }
+    }
+    if (!(c & FLAGSTAT_FUNMAP)) ++(s)->n_mapped[w];
+    if (c & FLAGSTAT_FDUP) ++(s)->n_dup[w];
+}
+
+#ifdef __GNUC__
+__attribute__((optimize("no-tree-vectorize")))
+#endif
 int samtools_flagstats(const uint16_t* array, uint32_t len, uint32_t* flags) {
     bam_flagstat_t* s = (bam_flagstat_t*)calloc(1, sizeof(bam_flagstat_t));
     for (int i = 0; i < len; ++i) {
-        SAMTOOLS_flagstat_loop(s, array[i]);
+        // SAMTOOLS_flagstat_loop(s, array[i]);
+        SAMTOOLS_func(s, array[i]);
     }
     flags[0] = s->n_read1[0]; // prevent optimzie away
     flags[1] += flags[0];
@@ -285,6 +310,102 @@ bool benchmarkMany(uint32_t n, uint32_t m, uint32_t iterations, FLAGSTATS_func f
     return isok;
 }
 
+bool benchmarkManyMemoryOptimized(const std::string& fn_name, uint32_t n, uint32_t m, uint32_t iterations, FLAGSTATS_func fn, bool verbose, bool test, bool tabular) {
+    std::vector<int> evts;
+// #ifdef ALIGN
+//     std::vector<std::vector<uint16_t,AlignedSTLAllocator<uint16_t,64>>> vdata(m, std::vector<uint16_t,AlignedSTLAllocator<uint16_t,64>>(n));
+// #else
+//     std::vector<std::vector<uint16_t>> vdata(m, std::vector<uint16_t>(n));
+// #endif
+// #ifdef ALIGN
+//     for(auto & x : vdata) {
+//       assert(get_alignment(x.data()) == 64);
+//     }
+// #endif
+
+    const uint32_t best_alignment = STORM_get_alignment();
+    STORM_ALIGN(64) uint16_t** vdata = (uint16_t**)STORM_aligned_malloc(best_alignment, m*sizeof(uint16_t*));
+    for (int i = 0; i < m; ++i)
+        vdata[i] = (uint16_t*)STORM_aligned_malloc(best_alignment, n*sizeof(uint16_t));
+
+    // if(verbose) {
+    //   printf("alignments: ");
+    //   for(auto & x : vdata) {
+    //     printf("%d ", get_alignment(x.data()));
+    //   }
+    //   printf("\n");
+    // }
+
+    printf("alignments: %d\n", best_alignment);
+
+    evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+    evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+    evts.push_back(PERF_COUNT_HW_BRANCH_MISSES);
+    evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
+    evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
+    evts.push_back(PERF_COUNT_HW_REF_CPU_CYCLES);
+    LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+    std::vector<unsigned long long> results; // tmp buffer
+    std::vector< std::vector<unsigned long long> > allresults;
+    results.resize(evts.size());
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 0xFFFF);
+
+    bool isok = true;
+    for (uint32_t i = 0; i < iterations; i++) {
+        for (size_t k = 0; k < m; k++) {
+            for(size_t k2 = 0; k2 < n ; k2++) { 
+                vdata[k][k2] = dis(gen); // random init.
+            }
+        }
+
+        std::vector<std::vector<uint32_t>> flags(m, std::vector<uint32_t>(16*2));
+        
+        unified.start();
+        for (size_t k = 0; k < m ; k++) {
+            fn(vdata[k], n, flags[k].data());
+        }
+        unified.end(results);
+        allresults.push_back(results);
+    }
+
+    std::vector<unsigned long long> mins = compute_mins(allresults);
+    std::vector<double> avg = compute_averages(allresults);
+    
+    if (verbose) {
+        printf("instructions per cycle %4.2f, cycles per 16-bit word:  %4.3f, "
+               "instructions per 16-bit word %4.3f \n",
+                double(mins[1]) / mins[0], double(mins[0]) / (n*m), double(mins[1]) / (n*m));
+        // first we display mins
+        printf("min: %8llu cycles, %8llu instructions, \t%8llu branch mis., %8llu "
+               "cache ref., %8llu cache mis.\n",
+                mins[0], mins[1], mins[2], mins[3], mins[4]);
+        printf("avg: %8.1f cycles, %8.1f instructions, \t%8.1f branch mis., %8.1f "
+               "cache ref., %8.1f cache mis.\n",
+                avg[0], avg[1], avg[2], avg[3], avg[4]);
+    } else {
+        printf("cycles per 16-bit word:  %4.3f; ref cycles per 16-bit word: %4.3f \n", double(mins[0]) / (n*m), double(mins[5]) / (n*m));
+    }
+
+    if (tabular) {
+        for (int i = 0; i < iterations; ++i) {
+            printf("%s\t%d\t", fn_name.c_str(), i);
+            printf("%4.2f\t%4.3f\t%4.3f\t",
+                    double(allresults[i][1]) / allresults[i][0], double(allresults[i][0]) / (n*m), double(allresults[i][1]) / (n*m));
+            printf("%llu\t%llu\t%llu\t%llu\t%llu\n",
+                    allresults[i][0], allresults[i][1], allresults[i][2], allresults[i][3], allresults[i][4]);
+        }
+    }
+
+    for (int i = 0; i < m; ++i)
+        STORM_aligned_free(vdata[i]);
+    STORM_aligned_free(vdata);
+
+    return isok;
+}
+
 void measureoverhead(uint32_t n, uint32_t iterations, bool verbose) {
     std::vector<int> evts;
     evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
@@ -307,6 +428,7 @@ void measureoverhead(uint32_t n, uint32_t iterations, bool verbose) {
     std::vector<unsigned long long> mins = compute_mins(allresults);
     std::vector<double> avg = compute_averages(allresults);
     printf("%-40s\t","nothing");    
+    
     if (verbose) {
         printf("instructions per cycle %4.2f, cycles per 16-bit word:  %4.3f, "
                "instructions per 16-bit word %4.3f \n",
@@ -323,6 +445,84 @@ void measureoverhead(uint32_t n, uint32_t iterations, bool verbose) {
     }
 }
 
+bool benchmarkMemoryCopy(const std::string& fn_name, uint32_t n, uint32_t m, uint32_t iterations, bool verbose, bool tabular) {
+    std::vector<int> evts;
+    const uint32_t best_alignment = STORM_get_alignment();
+    STORM_ALIGN(64) uint16_t** vdata = (uint16_t**)STORM_aligned_malloc(best_alignment, m*sizeof(uint16_t*));
+    for (int i = 0; i < m; ++i)
+        vdata[i] = (uint16_t*)STORM_aligned_malloc(best_alignment, n*sizeof(uint16_t));
+    STORM_ALIGN(64) uint16_t* dst = (uint16_t*)STORM_aligned_malloc(best_alignment, n*sizeof(uint16_t*));
+        
+    printf("alignments: %d\n", best_alignment);
+
+    evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+    evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+    evts.push_back(PERF_COUNT_HW_BRANCH_MISSES);
+    evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
+    evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
+    evts.push_back(PERF_COUNT_HW_REF_CPU_CYCLES);
+    LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+    std::vector<unsigned long long> results; // tmp buffer
+    std::vector< std::vector<unsigned long long> > allresults;
+    results.resize(evts.size());
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 0xFFFF);
+
+    bool isok = true;
+    for (uint32_t i = 0; i < iterations; i++) {
+        for (size_t k = 0; k < m; k++) {
+            for(size_t k2 = 0; k2 < n ; k2++) {
+                vdata[k][k2] = dis(gen); // random init.
+            }
+        }
+
+        // std::vector<std::vector<uint32_t>> flags(m, std::vector<uint32_t>(16*2));
+        
+        unified.start();
+        for (size_t k = 0; k < m ; k++) {
+            memcpy(dst, vdata[k], n*sizeof(uint16_t));
+        }
+        unified.end(results);
+        allresults.push_back(results);
+    }
+
+    std::vector<unsigned long long> mins = compute_mins(allresults);
+    std::vector<double> avg = compute_averages(allresults);
+    
+    if (verbose) {
+        printf("instructions per cycle %4.2f, cycles per 16-bit word:  %4.3f, "
+               "instructions per 16-bit word %4.3f \n",
+                double(mins[1]) / mins[0], double(mins[0]) / (n*m), double(mins[1]) / (n*m));
+        // first we display mins
+        printf("min: %8llu cycles, %8llu instructions, \t%8llu branch mis., %8llu "
+               "cache ref., %8llu cache mis.\n",
+                mins[0], mins[1], mins[2], mins[3], mins[4]);
+        printf("avg: %8.1f cycles, %8.1f instructions, \t%8.1f branch mis., %8.1f "
+               "cache ref., %8.1f cache mis.\n",
+                avg[0], avg[1], avg[2], avg[3], avg[4]);
+    } else {
+        printf("cycles per 16-bit word:  %4.3f; ref cycles per 16-bit word: %4.3f \n", double(mins[0]) / (n*m), double(mins[5]) / (n*m));
+    }
+
+    if (tabular) {
+        for (int i = 0; i < iterations; ++i) {
+            printf("%s\t%d\t", fn_name.c_str(), i);
+            printf("%4.2f\t%4.3f\t%4.3f\t",
+                    double(allresults[i][1]) / allresults[i][0], double(allresults[i][0]) / (n*m), double(allresults[i][1]) / (n*m));
+            printf("%llu\t%llu\t%llu\t%llu\t%llu\n",
+                    allresults[i][0], allresults[i][1], allresults[i][2], allresults[i][3], allresults[i][4]);
+        }
+    }
+
+    for (int i = 0; i < m; ++i) STORM_aligned_free(vdata[i]);
+    STORM_aligned_free(vdata);
+    STORM_aligned_free(dst);
+
+    return isok;
+}
+
 static void print_usage(char *command) {
     printf(" Try %s -n 100000 -i 15 -v \n", command);
     printf("-n is the number of 16-bit words \n");
@@ -335,9 +535,10 @@ int main(int argc, char **argv) {
     size_t m = 1;
     size_t iterations = 0; 
     bool verbose = false;
+    bool tabular = false;
     int c;
 
-    while ((c = getopt(argc, argv, "vhm:n:i:")) != -1) {
+    while ((c = getopt(argc, argv, "vthm:n:i:")) != -1) {
         switch (c) {
         case 'n':
             n = atoll(optarg);
@@ -347,6 +548,9 @@ int main(int argc, char **argv) {
             break;
         case 'v':
             verbose = true;
+            break;
+        case 't':
+            tabular = true;
             break;
         case 'h':
             print_usage(argv[0]);
@@ -373,20 +577,25 @@ int main(int argc, char **argv) {
       if(m*n < 1000000) iterations = 100;
       else iterations = 10;
     }
-    printf("n = %zu m = %zu \n", n, m);
-    printf("iterations = %zu \n", iterations);
+    
+    if (!tabular) {
+        printf("n = %zu m = %zu \n", n, m);
+        printf("iterations = %zu \n", iterations);
+    }
     if(n == 0) {
        printf("n cannot be zero.\n");
        return EXIT_FAILURE;
     }
 
-    size_t array_in_bytes = sizeof(uint16_t) * n * m;
-    if(array_in_bytes < 1024) {
-      printf("array size: %zu B\n", array_in_bytes);
-    } else if (array_in_bytes < 1024 * 1024) {
-      printf("array size: %.3f kB\n", array_in_bytes / 1024.);
-    } else {
-      printf("array size: %.3f MB\n", array_in_bytes / (1024 * 1024.));
+    if (!tabular) {
+        size_t array_in_bytes = sizeof(uint16_t) * n * m;
+        if(array_in_bytes < 1024) {
+        printf("array size: %zu B\n", array_in_bytes);
+        } else if (array_in_bytes < 1024 * 1024) {
+        printf("array size: %.3f kB\n", array_in_bytes / 1024.);
+        } else {
+        printf("array size: %.3f MB\n", array_in_bytes / (1024 * 1024.));
+        }
     }
 
     measureoverhead(n*m, iterations, verbose);
@@ -410,54 +619,82 @@ int main(int argc, char **argv) {
     #endif
 #endif
 
-    printf("libflagstats-scalar\t");
+    if (!tabular) printf("libflagstats-scalar\t");
     fflush(NULL);
     bool isok = benchmarkMany(n, m, iterations, FLAGSTAT_scalar, verbose, true);
     if (isok == false) {
         printf("Problem detected with %u.\n", 0);
     }
-    if (verbose)
+    if (verbose && !tabular)
         printf("\n");
 
-    printf("samtools\t");
+    if (!tabular) printf("samtools\t");
     fflush(NULL);
     isok = benchmarkMany(n, m, iterations, samtools_flagstats, verbose, true);
     if (isok == false) {
         printf("Problem detected with %u.\n", 0);
     }
-    if (verbose)
+    if (verbose && !tabular)
         printf("\n");
+
+    if (!tabular) printf("memcpy\t");
+    fflush(NULL);
+    isok = benchmarkMemoryCopy("memcpy", n, m, iterations, verbose, tabular);
+    if (isok == false) {
+        printf("Problem detected with %u.\n", 0);
+    }
+    if (verbose && !tabular)
+        printf("\n");
+    
 
     #if defined(STORM_HAVE_SSE42)
         if ((cpuid & STORM_CPUID_runtime_bit_SSE42)) {
-            printf("libflagstats-sse4.2\t");
+            if (!tabular) printf("libflagstats-sse4.2\t");
             fflush(NULL);
             bool isok = benchmarkMany(n, m, iterations, FLAGSTAT_sse4, verbose, true);
             if (isok == false) {
                 printf("Problem detected with %u.\n", 0);
             }
-            if (verbose) printf("\n");
+            if (verbose && !tabular) printf("\n");
 
-            printf("libflagstats-sse4.2-optimized\t");
+            if (!tabular) printf("libflagstats-sse4.2-optimized\t");
             fflush(NULL);
             isok = benchmarkMany(n, m, iterations, FLAGSTAT_sse4_improved, verbose, true);
             if (isok == false) {
                 printf("Problem detected with %u.\n", 0);
             }
-            if (verbose) printf("\n");
+            if (verbose && !tabular) printf("\n");
+
+            if (!tabular) printf("libflagstats-sse4.2-optimized-aligned\t");
+            fflush(NULL);
+            isok = benchmarkManyMemoryOptimized("libflagstats-sse4.2-optimized-aligned", n, m, iterations, FLAGSTAT_sse4_improved, verbose, true, tabular);
+            if (isok == false) {
+                printf("Problem detected with %u.\n", 1);
+            }
+            if (verbose && !tabular)
+                printf("\n");
         }
     // }
     #endif
 
     #if defined(STORM_HAVE_AVX2)
         if ((cpuid & STORM_CPUID_runtime_bit_AVX2)) {
-            printf("libflagstats-avx2\t");
+            if (!tabular) printf("libflagstats-avx2\t");
             fflush(NULL);
             bool isok = benchmarkMany(n, m, iterations, FLAGSTAT_avx2, verbose, true);
             if (isok == false) {
                 printf("Problem detected with %u.\n", 1);
             }
-            if (verbose)
+            if (verbose && !tabular)
+                printf("\n");
+
+            if (!tabular) printf("libflagstats-avx2-aligned\t");
+            fflush(NULL);
+            isok = benchmarkManyMemoryOptimized("libflagstats-avx2-aligned", n, m, iterations, FLAGSTAT_avx2, verbose, true, tabular);
+            if (isok == false) {
+                printf("Problem detected with %u.\n", 1);
+            }
+            if (verbose && !tabular)
                 printf("\n");
         }
     // }
@@ -465,34 +702,34 @@ int main(int argc, char **argv) {
 
     #if defined(STORM_HAVE_AVX512)
         if ((cpuid & STORM_CPUID_runtime_bit_AVX512BW)) {
-            printf("libflagstats-avx512bw\t");
+            if (!tabular) printf("libflagstats-avx512bw\t");
             fflush(NULL);
             bool isok = benchmarkMany(n, m, iterations, FLAGSTAT_avx512, verbose, true);
             if (isok == false) {
                 printf("Problem detected with %u.\n", 2);
             }
-            if (verbose) printf("\n");
+            if (verbose && !tabular) printf("\n");
 
-            printf("libflagstats-avx512bw-improved\t");
+            if (!tabular) printf("libflagstats-avx512bw-improved\t");
             fflush(NULL);
             isok = benchmarkMany(n, m, iterations, FLAGSTAT_avx512_improved, verbose, true);
             if (isok == false) {
                 printf("Problem detected with %u.\n", 2);
             }
-            if (verbose) printf("\n");
+            if (verbose && !tabular) printf("\n");
 
-            printf("libflagstats-avx512bw-improved2\t");
+            if (!tabular) printf("libflagstats-avx512bw-improved2\t");
             fflush(NULL);
             isok = benchmarkMany(n, m, iterations, FLAGSTAT_avx512_improved2, verbose, true);
             if (isok == false) {
                 printf("Problem detected with %u.\n", 2);
             }
-            if (verbose) printf("\n");
+            if (verbose && !tabular) printf("\n");
         // }
         }
     #endif
 
-    if (!verbose)
+    if (!verbose && !tabular)
         printf("Try -v to get more details.\n");
 
     return EXIT_SUCCESS;
